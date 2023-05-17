fae79c8a3446f209448f0c343c2a5dc4fe9f0626
raj-28 <airbus.a747@gmail.com>
2023-05-17 11:33:19 +0530

    new filr

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TodoInput = ({ todo, setTodo, addTodo }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        type="text"
+        name="todo"
+        value={todo}
+        placeholder="create a new todo"
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+
+      <button className="add-button"  onClick={addTodo}>
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default TodoInput;
